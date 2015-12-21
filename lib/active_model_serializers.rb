fc04b1400a623b3fefc95bc1f5e5672ccbf61a8a
Benjamin Fleischer <github@benjaminfleischer.com>
2015-12-21 00:12:52 -0600

    Fix load-order issues

--- lib/active_model_serializers.rb
+++ lib/active_model_serializers.rb
@@ -2,17 +2,12 @@ require 'active_model'
 require 'active_support'
 require 'action_controller'
 require 'action_controller/railtie'
-require 'active_model/serializer/version'
-require 'active_model/serializer'
-require 'active_model_serializers/railtie'
 module ActiveModelSerializers
   extend ActiveSupport::Autoload
   autoload :Model
   autoload :Callbacks
+  autoload :Deserialization
   autoload :Logging
-
-  require 'active_model/serializable_resource'
-  require 'action_controller/serialization'
 
   mattr_accessor(:logger) { ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT)) }
 
@@ -20,9 +15,9 @@ module ActiveModelSerializers
     ActiveModel::Serializer.config
   end
 
-  extend ActiveSupport::Autoload
-  autoload :Model
-  autoload :Callbacks
-  autoload :Deserialization
-  autoload :Logging
+  require 'active_model/serializer/version'
+  require 'active_model/serializer'
+  require 'active_model_serializers/railtie'
+  require 'active_model/serializable_resource'
+  require 'action_controller/serialization'
 end
