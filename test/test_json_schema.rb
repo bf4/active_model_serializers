341d78c896e2bf3fbbb1eb1a189100dcdd9d25f0
Benjamin Fleischer <github@benjaminfleischer.com>
2017-01-25 21:27:47 -0600

    Validate json schema

--- test/test_json_schema.rb
+++ test/test_json_schema.rb
@@ -3,42 +3,63 @@ require 'json_schema'
 
 require 'minitest'
 class JsonSchemaTest < Minitest::Test
-
   def setup
     super
-    test_dir = File.expand_path('..', __FILE__)
+    init_schemas
+  end
+
+  class JsonApiLinkTest < JsonSchemaTest
+    def test_link_string_is_valid
+      data = "http://www.example.com"
+      assert_schema_definition('link', data)
+    end
+
+    def test_link_object_with_href_is_valid
+      data = { "href" => "http://www.example.com" }
+      assert_schema_definition('link', data)
+    end
+
+    def test_link_object_without_href_is_invalid
+      data = { "not_a_href" => "http://www.example.com" }
+      error_matcher = /\"href\" wasn't supplied/
+      refute_schema_definition('link', data, error_matcher)
+    end
+
+    def test_link_object_href_not_a_uri_is_invalid
+      data = { "href" => "http://www.example.com[]" }
+      assert_raises(URI::InvalidURIError) { URI.parse(data["href"]) }
+      error_matcher = /not a valid uri/
+      refute_schema_definition('link', data, error_matcher)
+    end
+  end
+
+  private
+
+  attr_reader :schemas
+
+  require 'pathname'
+  TEST_DIR = Pathname File.expand_path('..', __FILE__)
+  private_constant :TEST_DIR
+
+  def init_schemas
     @document_store = JsonSchema::DocumentStore.new
-    schema_directory = File.join(test_dir, 'schema/schemata')
+    schema_directory = TEST_DIR.join('schema/schemata')
     @schemas = {}
-    Dir.glob("#{schema_directory}/**/*.json").each do |path|
+    Dir.glob(schema_directory.join("**/*.json")).each do |path|
       schema_data = JSON.parse(File.read(path))
       extra_schema = JsonSchema.parse!(schema_data)
-      @schemas[File.basename(path).sub(/\.json\z/, '')] = schema_data
+      definition_name = File.basename(path).sub(/\.json\z/, '')
+      @schemas[definition_name] = schema_data
       @document_store.add_schema(extra_schema)
     end
   end
 
-  def test_link_string_is_valid
-    data = "http://www.example.com"
-    assert_schema_definition('link', data)
-  end
+  def assert_schema_definition(definition, data)
+    schema = validator_for_definition(definition)
 
-  def test_link_object_with_href_is_valid
-    data = { "href" => "http://www.example.com" }
-    assert_schema_definition('link', data)
-  end
+    status, errors = schema.validate(data)
 
-  def test_link_object_without_href_is_invalid
-    data = { "not_a_href" => "http://www.example.com" }
-    error_matcher = /\"href\" wasn't supplied/
-    refute_schema_definition('link', data, error_matcher)
-  end
-
-  def test_link_object_href_not_a_uri_is_invalid
-    data = { "href" => "http://www.example.com[]" }
-    assert_raises(URI::InvalidURIError) { URI.parse(data["href"]) }
-    error_matcher = /not a valid uri/
-    refute_schema_definition('link', data, error_matcher)
+    assert status, schema_error_message(errors)
   end
 
   def refute_schema_definition(definition, data, error_matcher)
@@ -50,16 +71,8 @@ class JsonSchemaTest < Minitest::Test
     assert_match error_matcher, schema_error_message(errors)
   end
 
-  def assert_schema_definition(definition, data)
-    schema = validator_for_definition(definition)
-
-    status, errors = schema.validate(data)
-
-    assert status, schema_error_message(errors)
-  end
-
   def validator_for_definition(definition)
-    schema_data = @schemas.fetch(definition)
+    schema_data = schemas.fetch(definition)
     ## parse the schema - raise SchemaError if it's invalid
     schema = JsonSchema.parse!(schema_data)
     # expand $ref nodes - raise SchemaError if unable to resolve
